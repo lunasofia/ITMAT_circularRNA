fe56014c1ebb7ae6c24b120b1fe370f1f00c5333
S. Luna Frank-Fischer <luna.sofia.ff@gmail.com>
2014-06-30 11:53:44 -0400

    updated to keep counter and not print craziness

--- exonExonCrossingCounter.pl
+++ exonExonCrossingCounter.pl
@@ -27,51 +27,51 @@ my %exonLengths = ();
 my $QNAME = 0;
 my $RNAME = 2;
 my $POS = 3;
+my $QUAL = 4;
 my $CIGAR = 5;
 my $SEQ = 9;
+
 my $MIN_OVERLAP = 10;
+my $MAX_QUALITY = 35; #0 is the best quality
+
+my $matchCount = 0;
+
 open my $sam_fh, '<', $SAM_FILE;
 while(my $line = <$sam_fh>) {
-    if($line =~ /^@/) { 
-	print "skipped header\n";
-	next;
-    }
+    if($line =~ /^@/) { next; }
     chomp($line);
 
     my @fieldVals = split(" ", $line);
 
     my @nameArr = split(/\./, $fieldVals[$RNAME]);
-    if($#nameArr != 2) {
-#	print "invalid name field ($fieldVals[$RNAME])\n";
-	next;
-    }
+    if($#nameArr != 2) { next; }
 
-    my $key = &makeExonLenKey(">$nameArr[0]", $nameArr[1]);
+    if($fieldVals[$QUAL] > $MAX_QUALITY) { next; }
+
+    my $firstExonKey = &makeExonLenKey(">$nameArr[0]", $nameArr[1]);
     my $firstExonLen = 
-	$exonLengths{ $key };
-    if(!$firstExonLen) { 
-#	print "no matching exon in length database ($fieldVals[$RNAME])\n";
-	next; 
-    }
-    
+	$exonLengths{ $firstExonKey }->[2];
+    if(!$firstExonLen) { next; }
+
+    my $secondExonKey = &makeExonLenKey(">$nameArr[0]", $nameArr[2]);
+
     my $refAlignLength = getRefAlignLength($fieldVals[$CIGAR]);
 
     my $firstExOverlap = $firstExonLen - $fieldVals[$POS]; # add 1??
-    if($firstExOverlap < $MIN_OVERLAP) { 
-#	print "not enough overlap on first exon. first overlap: $firstExOverlap\n";
-	next; 
-    }
-    if($refAlignLength - $firstExOverlap < $MIN_OVERLAP) {
-#	print "not enough overlap on second exon. first overlap: $firstExOverlap\n";
-	next; 
-    }
+    if($firstExOverlap < $MIN_OVERLAP) { next; }
+    if($refAlignLength - $firstExOverlap < $MIN_OVERLAP) { next; }
 
-# TODO: figure out better output format!
-    print "MATCH! $fieldVals[$RNAME] $fieldVals[$POS]";
-    print "$fieldVals[$CIGAR] $fieldVals[$QNAME] $fieldVals[$SEQ]\n";
-    
-
+    print "MATCH!\n";
+    for(my $i = 0; $i <= $SEQ; $i++) {
+	print "$fieldVals[$i]\n";
+    }
+    print "Overlap on first exon: $firstExOverlap\n";
+    print "1: $exonLengths{ $firstExonKey }->[0] to $exonLengths{ $firstExonKey }->[1]\n";
+    print "2: $exonLengths{ $secondExonKey }->[0] to $exonLengths{ $secondExonKey }->[1]\n\n";
+    $matchCount++;
 }
+
+print "$matchCount matches\n";
 close $sam_fh;
 
 # Given a CIGAR string, returns the length along
@@ -115,11 +115,17 @@ sub getExonLengths {
 	my @namelinevals = split(" ", $nameline);
 	
 	my $key = makeExonLenKey($namelinevals[$GENE_NAME], $namelinevals[$EXON_NUM]);
-	my $value = $namelinevals[$EXON_END] - $namelinevals[$EXON_START];
+	my $value_arr = &makeExonLenVal($namelinevals[$EXON_START], $namelinevals[$EXON_END]);
 	
-	$exonLengths{ $key } = $value;
+	$exonLengths{ $key } = $value_arr;
     }
     close $exons_fh;
+}
+
+sub makeExonLenVal {
+    my @valArr = @_;
+    push @valArr, $_[1] - $_[0];
+    return \@valArr;
 }
 
 # Given a gene name and exon number, returns the map
