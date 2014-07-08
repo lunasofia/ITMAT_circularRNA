9abce5aaa56a84ebacf57a085787628e6349f0cf
S. Luna Frank-Fischer <luna.sofia.ff@gmail.com>
2014-07-08 13:32:05 -0400

    Wrote (but have not tested) script to filter out entries from fastq based on list of IDs to throw out.

---
+++ removeSetFromFQ.pl
@@ -0,0 +1,37 @@
+# File: removeSetFromFQ.pl
+# Author: S. Luna Frank-Fischer
+# ITMAT at UPenn
+# -------------------------------------------
+# Takes in as the first parameter a fastq files.
+# Takes in as the second parameter a file of
+# ID for searches (separated by newlines, no @
+# symbol prefix).
+#
+# Outputs a fastq with the entries corresponding
+# to those IDs removed.
+
+my ($FQ_FILE, $IDLIST_FILE) = @ARGV;
+
+my %idlist = ();
+
+# Read list of IDs into hash
+open my $idlist_fh, '<', $IDLIST_FILE;
+while(<$idlist_fh>) {
+    $idlist{ $_ } = 1;
+}
+close $idlist_fh;
+
+my $FQ_NUMLINES = 4;
+open my $fq_fh, '<', $FQ_FILE;
+while(<$fq_fh>) {
+    # skip if not an ID line or ID is in map
+    next if($. % $FQ_NUMLINES != 1);
+    next if($idlist{ $_ });
+
+    print $line;
+    for(my $i = 1; $i < $FQ_NUMLINES; $i++) {
+	my $contentLine = <$fq_fh>;
+	print "$contentLine";
+    }
+}
+close $fq_fh;
