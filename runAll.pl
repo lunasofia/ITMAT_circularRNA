9b0ca0b98a6b9cdf729ed63b3ec4c903127f9ffb
S. Luna Frank-Fischer <luna.sofia.ff@gmail.com>
2014-07-10 16:58:01 -0400

    Starting to write a script to run the whole pipeline. This will take a while, and involve modifications to other parts of the pipeline. Currently have only some of the header comments.

---
+++ runAll.pl
@@ -0,0 +1,9 @@
+# File: runAll.pl
+# Author: S. Luna Frank-Fischer
+# ITMAT at UPenn
+# ----------------------------------------
+# Runs the full pipeline to search for shuffled exons.
+# 
+# The directories must be arranged as specified in README.txt.
+#
+# 
