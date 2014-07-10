d2dfdf2d33896af3480ab6a93970e07b2876cad6
S. Luna Frank-Fischer <luna.sofia.ff@gmail.com>
2014-07-10 11:08:17 -0400

    Made early version of script to run the crossing filter for all given IDs

---
+++ runCrossFilter.pl
@@ -0,0 +1,22 @@
+# File: runCrossFilter.pl
+# Author: S. Luna Frank-Fischer
+# ITMAT at UPenn
+# ----------------------------------
+# Selects the desired files from the SAM output
+# of BWA. First uses grep to find the 100M matches
+# and then calls exonBoundaryCrossFilter.pl to
+# select only the matches which cross reverse
+# exon-exon boundaries by enough base pairs.
+#
+# This is very much a beta-version, and will be
+# improved and generalized for the full pipeline
+# script.
+
+use strict;
+
+foreach my $id (@ARGV) {
+    `grep "100M" unnormalized_datasets/$id/BWA_aln.sam > unnormalized_datasets/$id/100M_aln.sam`;
+    print "finished grep call for $id\n";
+    `perl scripts/exonBoundaryCrossFilter.pl exon_info_docs/mm9_ucsc_exons_info.txt unnormalized_datasets/$id/100M_aln.sam > unnormalized_datasets/$id/finalMatches.sam`;
+    print "finished full selection for $id\n";
+}
