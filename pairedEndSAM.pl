200b3655c151a18ecaf029d75ba05fd638034114
S. Luna Frank-Fischer <luna.sofia.ff@gmail.com>
2014-07-22 15:51:22 -0400

    Added functionality to deal with multiple of each type of SAM file

--- pairedEndSAM.pl
+++ pairedEndSAM.pl
@@ -12,14 +12,14 @@ use strict;
 use warnings;
 use Getopt::Long;
 
-my ($CROSS_SAM, $REG_SAM, $BOUNDARY, $help); 
+my (@CROSS_SAM_FILES, @REG_SAM_FILES, $BOUNDARY, $help); 
 
 GetOptions('help|?' => \$help,
-	   'crossing-sam-file=s' => \$CROSS_SAM,
-	   'regular-sam-file=s' => \$REG_SAM,
+	   'crossing-sam-file=s' => \@CROSS_SAM_FILES,
+	   'regular-sam-file=s' => \@REG_SAM_FILES,
 	   'boundary-name=s' => \$BOUNDARY);
 &usage if $help;
-&usage unless ($CROSS_SAM && $REG_SAM && $BOUNDARY);
+&usage unless ($CROSS_SAM_FILES[0] && $REG_SAM_FILES[0] && $BOUNDARY);
 
 # Constants for reading SAM files
 my $S_QNAME = 0;
@@ -29,32 +29,35 @@ my $S_RNAME = 2;
 # events and put them into a hash.
 my %crossingEvents = ();
 
-open my $cross_sam_fh, '<', $CROSS_SAM;
-while(my $line = <$cross_sam_fh>) {
-    chomp($line);
-    my @samVals = split("\t", $line);
-    next unless $samVals[$S_RNAME] eq $BOUNDARY;
-    
-    # add to hash
-    $crossingEvents{ $samVals[$S_QNAME] } = 1;
-
-    # print out SAM line
-    print "$line\n";
+foreach my $FILE (@CROSS_SAM_FILES) {
+    open my $cross_sam_fh, '<', $FILE or die "ERROR: could not open file $FILE\n";
+    while(my $line = <$cross_sam_fh>) {
+	chomp($line);
+	my @samVals = split("\t", $line);
+	next unless $samVals[$S_RNAME] eq $BOUNDARY;
+	
+	# add to hash
+	$crossingEvents{ $samVals[$S_QNAME] } = 1;
+	
+	# print out SAM line
+	print "$line\n";
+    }
+    close $cross_sam_fh;
 }
-close $cross_sam_fh;
 
-# Read through full SAM file, printing all with IDs that
-# match the hash.
-open my $reg_sam_fh, '<', $REG_SAM;
-while(my $line = <$reg_sam_fh>) {
-    chomp($line);
-    my @samVals = split("\t", $line);
-    next unless $crossingEvents{ $samVals[$S_QNAME] };
-    
-    print "$line\n";
+foreach my $FILE (@REG_SAM_FILES) {    
+    # Read through full SAM file, printing all with IDs that
+    # match the hash.
+    open my $reg_sam_fh, '<', $FILE or die "ERROR: could not open file $FILE\n";
+    while(my $line = <$reg_sam_fh>) {
+	chomp($line);
+	my @samVals = split("\t", $line);
+	next unless $crossingEvents{ $samVals[$S_QNAME] };
+	
+	print "$line\n";
+    }
+    close $reg_sam_fh;
 }
-close $reg_sam_fh;
-
 
 sub usage {
 die "
