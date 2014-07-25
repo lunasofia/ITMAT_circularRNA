a1753b67f00b19d97171442c665ab9cacd22ef40
S. Luna Frank-Fischer <luna.sofia.ff@gmail.com>
2014-07-25 16:58:00 -0400

    Haven't really done anything; want to get formatting down for other files first

---
+++ compareToCircBase.pl
@@ -0,0 +1,7 @@
+# File: compareToCircBase.pl
+# Author: S. Luna Frank-Fischer
+# ITMAT at UPenn
+# --------------------------------------
+# This is written to compare our findings to the circBase database of
+# circular RNA. The input format for the circBase information is the
+# same as the spreadsheet downloadable from the circBase website.
