1cf3856c142bd434744296f57a608c32039993d4
S. Luna Frank-Fischer <luna.sofia.ff@gmail.com>
2014-07-29 09:56:27 -0400

    Added ability to specify which columns should be used for sorting

--- sortBySum.pl
+++ sortBySum.pl
@@ -6,9 +6,34 @@
 # created by combineColumns.pl), prints out the
 # same spreadsheet with entries sorted in order of
 # the sum of the columns (with highest first).
+# Takes in a set of columns to sum and sort by.
 
 use strict;
 use warnings;
+use Getopt::Long;
+my ($help, @COLS_TO_SUM, $SPREADSHEET);
+GetOptions('help|?' => \$help,
+	   'column-to-sum=i' => \@COLS_TO_SUM,
+	   'spreadsheet=s' => \$SPREADSHEET);
+
+&usage if $help;
+&usage unless ($COLS_TO_SUM[0] && $SPREADSHEET);
+
+sub usage {
+    die "
+ Given a tab-separated spreadsheet (in the format
+ created by combineColumns.pl), prints out the
+ same spreadsheet with entries sorted in order of
+ the sum of the columns specified. Leaves the first
+ line alone.
+
+ Necessary flags:
+ --column-to-sum (-c)
+ --spreadsheet (-s)
+
+ USAGE: perl sortBySum.pl -c 2 -c 3 -c 4 -c 8 -s filename.txt
+   
+"}
 
 # Hash from line to sum of the frequencies for that
 # line
@@ -25,7 +50,7 @@ while(my $line = <>) {
     }
 
     my $sum = 0;
-    for(my $i = 1; $i <= $#vals; $i++) {
+    foreach my $i (@COLS_TO_SUM) {
 	$sum += $vals[$i];
     }
     
