784fc38d15e358f08e52cacbf60ca7f06594ab3b
S. Luna Frank-Fischer <luna.sofia.ff@gmail.com>
2014-08-04 13:32:25 -0400

    added ability to also fill in method using duplication listings

--- fillInTissueTypeInfo.pl
+++ fillInTissueTypeInfo.pl
@@ -7,23 +7,57 @@
 
 use strict;
 use warnings;
+use Getopt::Long;
 
-my ($TISSUE_FILE, $DUP_FILE, $SPREADSHEET) = @ARGV;
+my ($TISSUE_FILE, $RvP_FILE, $DUP_FILE, $SPREADSHEET, $help);
+GetOptions('help|?' => \$help,
+	   'tissue-file=s' => \$TISSUE_FILE,
+	   'ribo-poly-file=s' => \$RvP_FILE,
+	   'dup-file=s' => \$DUP_FILE,
+	   'spreadsheet=s' => \$SPREADSHEET);
+
+&usage if $help;
+&usage unless ($TISSUE_FILE && $RvP_FILE && $DUP_FILE && $SPREADSHEET);
+
+sub usage {
+    die "
+ Necessary flags:
+ --tissue-file
+ --ribo-poly-file
+ --dup-file
+ --spreadsheet
+
+"}
 
 
 # read in tissue information
-my %IDtoInfo = ();
+my %IDtoTissue = ();
 
 open my $tissue_fh, '<', $TISSUE_FILE or die "could not open $TISSUE_FILE\n";
 while(my $line = <$tissue_fh>) {
     chomp($line);
     my @vals = split("\t", $line);
 
-    $IDtoInfo{ $vals[0] } = \@vals;
+    $IDtoTissue{ $vals[0] } = \@vals;
 }
 close $tissue_fh;
 
-# Load primary ID information
+
+
+# read in ribo vs poly information
+my %IDtoRvP = ();
+
+open my $rvp_fh, '<', $RvP_FILE or die "could not open $RvP_FILE\n";
+while(my $line = <$rvp_fh>) {
+    chomp($line);
+    my @vals = split("\t", $line);
+
+    $IDtoRvP{ $vals[0] } = \@vals;
+}
+close $rvp_fh;
+
+
+# read in primary ID information
 my %idToPrimary = ();
 
 open my $dup_fh, '<', $DUP_FILE or die "could not open $DUP_FILE\n";
@@ -59,22 +93,39 @@ while(my $line = <$spreadsheet_fh>) {
     $id = substr($id, 0, -2) if ($id =~ /_1$/);
     my $primaryID = $idToPrimary{ $id };
 
-    my $infoRef = $IDtoInfo{ $id };
-    $infoRef = $IDtoInfo{ $primaryID } if $primaryID;
+    my $rvpRef = $IDtoRvP{ $id };
+    $rvpRef = $IDtoRvP{ $primaryID } if $primaryID;
 
-    if($infoRef) {
+    my $tissueRef = $IDtoTissue{ $id };
+    $tissueRef = $IDtoTissue{ $primaryID } if $primaryID;
+
+
+    if($rvpRef) {
 	# look at each column with procedure info, fill in unless
 	# that column has a '-'.
 	my @methods;
 
-	my @info = @$infoRef;
+	my @info = @$rvpRef;
 
 	for(my $i = 2; $i <= $#info; $i++) {
 	    push @methods, $info[$i] unless ($info[$i] eq '-' || $info[$i] eq '');
 	}
 
 	# write in as method if anything was found
-	$vals[3] = join(",", @methods) if $methods[0];
+	$vals[2] = join(",", @methods) if $methods[0];
+    }
+
+    if($tissueRef) {
+	my @tissues;
+       
+	my @info = @$tissueRef;
+	
+	for(my $i = 2; $i <= $#info; $i++) {
+	    push @tissues, $info[$i] unless ($info[$i] eq '-' || $info[$i] eq '');
+	}
+	
+	# write in as tissue if anything was found
+	$vals[3] = join(",", @tissues) if $tissues[0];
     }
 
     print join("\t", @vals), "\n";
